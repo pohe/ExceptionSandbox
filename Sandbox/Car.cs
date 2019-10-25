8f12274276ad91440db170723ef3c4431756c236
Poul Henriksen <pohe@easj.dk>
2019-10-25 10:28:00 +0200

    Add project files.

---
+++ Sandbox/Car.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public class Car
+    {
+        private string _regNr;
+
+        public string Regnr
+        {
+            get { return _regNr; }
+            set { _regNr = value; }
+        }
+
+        public string Model { get; set; }
+
+        public Car(string model)
+        {
+            Model = model;
+        }
+
+        public Car(string regNr, string model)
+        {
+            _regNr = regNr;
+            Model = model;
+        }
+
+        public override string ToString()
+        {
+            return "Model " + Model + " regnr " + Regnr;
+        }
+        
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            //try
+            //{
+            //    string i = "";
+            //    Console.WriteLine("Indtast tal 1:");
+            //    i = Console.ReadLine();
+            //    int tal1 = Convert.ToInt32(i);
+
+            //    Console.WriteLine("Indtast tal 2:");
+            //    i = Console.ReadLine();
+            //    int tal2 = Convert.ToInt32(i);
+            //    int resultat = tal1 / tal2;
+            //    //Console.WriteLine("Resultatet af heltalsdivision er "
+            //    //                  + resultat.ToString("n0"));
+            //    Console.WriteLine("Resultatet af heltalsdivision er "
+            //                      + resultat);
+            //}
+            //catch (DivideByZeroException dex)
+            //{
+            //    Console.WriteLine("Hov du dividerede med nul!");
+            //}
+            //catch (FormatException fex)
+            //{
+            //    Console.WriteLine("Input er i forkert format!");
+            //}
+            //catch (OverflowException oex)
+            //{
+            //    Console.WriteLine("Input tallet er for stort!");
+            //}
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Hov der skete en fejl!" + e.Message);
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("I denne blok kommer jeg altid!");
+            //}
+
+            Console.WriteLine("Indlæs nummerplade:");
+            string regnr = Console.ReadLine();
+            Console.WriteLine("Indlæs model");
+            string model = Console.ReadLine();
+
+            Car c = new Car(model, regnr);
+
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
